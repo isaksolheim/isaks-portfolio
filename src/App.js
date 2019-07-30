314afd9d222286c2a13e7d883d2488ac97c75869
wickdeeb <isak.m.h.s@gmail.com>
2019-07-30 15:14:47 +0200

    add header

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './styles/main.scss';
 import Navbar from './components/Navbar';
 import About from './components/About';
 import Skills from './components/Skills';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Header />
       <About />
       <Skills />
     </div>
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Header() {
+  return(
+    <header>
+      <p>
+        Hi, I'm Isak Solheim, a front-end
+        developer focused on creating beautiful and
+        user friendly applications.
+      </p>
+    </header>
+  );
+}
+
+export default Header;
