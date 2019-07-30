06d104c315c567ea4a580f61b968adb8a46bc679
wickdeeb <isak.m.h.s@gmail.com>
2019-07-30 18:11:52 +0200

    add parallax fade effect on header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,44 @@
 import React from 'react';
 
-function Header() {
-  return(
-    <header>
-      <p>
-        Hi, I'm Isak Solheim, a front-end
-        developer focused on creating beautiful and
-        user friendly applications.
-      </p>
-    </header>
-  );
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentScrollHeight: 0,
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll = () => {
+    if (window.scrollY < 204) {
+      this.setState({ currentScrollHeight: window.scrollY });
+    }
+  }
+
+  render() {
+    const opacity = 1 - (this.state.currentScrollHeight / 130);
+    console.log(opacity);
+    let fadeStyle;
+    if (opacity < 0) {
+      fadeStyle = { color: 'rgba(85, 85, 85, 0)' }
+    } else {
+      fadeStyle = { color: `rgba(85, 85, 85, ${opacity})` };
+    }
+    return(
+      <header style={fadeStyle}>
+        {console.log(fadeStyle)}
+        <p>
+          Hi, I'm Isak Solheim, a <b>front-end
+          developer</b> focused on creating <b>beautiful</b> and
+          <b> user friendly</b> applications while writing <b>clean code</b>.
+        </p>
+      </header>
+    );
+  }
 }
 
 export default Header;
