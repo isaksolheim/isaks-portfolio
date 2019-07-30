23479e89484fa5c6f884936d2feb7e9ed5b875f9
wickdeeb <isak.m.h.s@gmail.com>
2019-07-30 17:10:39 +0200

    add footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Header from './components/Header';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <Projects />
       <Skills />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return(
+    <footer>
+      <a href="https://github.com/wickdeeb"><i className="fab fa-github"></i></a>
+      <a href="mailto:isak.m.h.s@gmail.com"><i className="fas fa-envelope"></i></a>
+      <p>2019 - Created by Isak Solheim</p>
+    </footer>
+  );
+}
+
+export default Footer;
