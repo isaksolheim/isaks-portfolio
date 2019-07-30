724a557f517cdc7ea6892b70acdd7a498d685306
wickdeeb <isak.m.h.s@gmail.com>
2019-07-30 15:09:34 +0200

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './styles/main.scss';
 import Navbar from './components/Navbar';
+import About from './components/About';
+import Skills from './components/Skills';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <About />
+      <Skills />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,14 +6,8 @@ function Navbar() {
       <ul className="left">
         <li>isaks.io</li>
       </ul>
-      <ul className="center">
-        <li>About</li>
-        <li>Skills</li>
-        <li>Projects</li>
-      </ul>
       <ul className="right">
-        <li>Github</li>
-        <li>Resume</li>
+        <li><i className="fas fa-bars"></i></li>
       </ul>
     </nav>
   );
